25eb26de9a04cc9f2544ff58d30cb9a3e449d949
yuri-dev20 <yuriglauco945@gmail.com>
2025-10-28 16:25:31 -0300

    made humanChoice case-insensitive

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 let getComputerChoice = () => {
     // Returns either 0, 1 or 2
     let computerChoice = Math.floor(Math.random() * 3);
@@ -14,6 +17,13 @@ let getComputerChoice = () => {
 }
 
 let getHumanChoice = () => {
-    return prompt("Please enter a choice (Rock, Paper or Scissors)");
+    let humanChoice = prompt("Please enter a choice (Rock, Paper or Scissors)");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
 }
 
+console.log(getHumanChoice());
+
+// function playRound(humanChoice, computerChoice) {
+    
+// }
